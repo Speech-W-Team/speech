7e75800fadd08f1c9c0a703024b12701a9201679
Usman Akhmedov <akhme.v@gmail.com>
2023-08-26 04:18:27 +0300

    Fixed errors

--- shared/crypto/domain/src/iosMain/kotlin/wtf/speech/vault/crypto/domain/usecases/hash.kt
+++ shared/crypto/domain/src/iosMain/kotlin/wtf/speech/vault/crypto/domain/usecases/hash.kt
@@ -1,18 +1,6 @@
 package wtf.speech.vault.crypto.domain.usecases
 
-actual suspend fun ripemd160(data: ByteArray): ByteArray {
-    TODO("Not yet implemented")
-}
-
-actual suspend fun sha256(data: ByteArray): ByteArray {
-    TODO("Not yet implemented")
-}
-
 actual suspend fun base58Encode(data: ByteArray): String {
-    TODO("Not yet implemented")
-}
-
-actual suspend fun keccak256(data: ByteArray): ByteArray {
     TODO("Not yet implemented")
 }
 
