1ca5c714fddb72409d3bf8216bda53fae766beb3
greenelephante <greenelephant112233@gmail.com>
2023-09-14 18:35:51 +0200

    VAUL-37: create generators for ethereum blockchain

---
+++ crypto/src/blockchains/ethereum_blockchains.rs
@@ -0,0 +1,29 @@
+use secp256k1::{PublicKey, Secp256k1, SecretKey};
+use sha3::{Digest, Keccak256};
+use hex::encode;
+use rand::Rng;
+
+pub fn generate_keypair() -> Result<(SecretKey, PublicKey), &'static str> {
+    let mut rng = rand::thread_rng();
+
+    let private_key_bytes: [u8; 32] = rng.gen();
+
+    let secp = Secp256k1::new();
+    let private_key =
+        SecretKey::from_slice(&private_key_bytes).map_err(|_| "Invalid private key")?;
+
+    let public_key = PublicKey::from_secret_key(&secp, &private_key);
+
+    Ok((private_key, public_key))
+}
+
+pub fn get_address(public_key_bytes: &Vec<u8>) -> String {
+    let mut hasher = Keccak256::new();
+    hasher.update(&public_key_bytes[1..]);
+    let hash = hasher.finalize();
+    let hash = &hash[12..];
+
+    let address = encode(hash);
+
+    address
+}
---
+++ crypto/tests/ethereum_blockchain_test.rs
@@ -0,0 +1,21 @@
+#[cfg(test)]
+mod tests {
+    use crypto::blockchains::ethereum_blockchains::*;
+
+    #[test]
+    fn test_generate_keypair() {
+        let (private_key, public_key) = generate_keypair().expect("Failed to generate keypair");
+
+        assert_eq!(private_key.to_string().len(), 64);
+        assert_eq!(public_key.serialize_uncompressed().len(), 65);
+    }
+
+    #[test]
+    fn test_get_address() {
+        let mut public_key_bytes: Vec<u8> = vec![4; 64];
+        public_key_bytes.insert(0, 4);
+        let address = get_address(&public_key_bytes);
+
+        assert_eq!(address.len(), 40);
+    }
+}
